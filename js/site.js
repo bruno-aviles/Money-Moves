e86363abd67eaeea84745216ff2d4b72beb53493
Bruno Aviles <bruno.aviles@yahoo.com>
2022-04-21 20:29:17 -0500

    create functionality for Total Principal, Total Interest, Total Cost displays

--- js/site.js
+++ js/site.js
@@ -1,24 +1,25 @@
-// Output include the following:
+// Payment Display:
 function monthlyPaymentDisplay(){
     let loanAmount = document.getElementById("loanAmount").value;
     let loanTerm = document.getElementById("loanTerm").value;
     let interestRate = document.getElementById("interestRate").value;
 
     let annualPercentageRate = monthlyInterestRate(interestRate);
-    let totalInterestAmount = interestAmount(loanAmount, annualPercentageRate);
-    let totalCost = totalAmount(loanAmount, totalInterestAmount);
 
     // Monthly Cost
     let monthlyPayment = ((Number(loanAmount) * Number(annualPercentageRate)) * Math.pow(1 + Number(annualPercentageRate), Number(loanTerm))) / (Math.pow(1 + Number(annualPercentageRate), Number(loanTerm)) - 1);
-    document.getElementById("monthlyPayment").innerHTML = monthlyPayment.toFixed(2);
+    document.getElementById("monthlyPayment").innerHTML = `$${monthlyPayment.toFixed(2)}`;
     
-    // alert(monthlyPayment);
-
-    // display total cost
-    document.getElementById("totalPrincipal").innerHTML = `$${Number(loanAmount).toFixed(2)}`;
-    document.getElementById("totalInterest").innerHTML = `$${Number(totalInterestAmount).toFixed(2)}`;
+    // Total Loan Cost
+    let totalCost = totalAmount(loanTerm, monthlyPayment);
     document.getElementById("totalCost").innerHTML = `$${Number(totalCost).toFixed(2)}`;
 
+    // Total Interest Cost
+    let totalInterestAmount = interestAmount(totalCost, loanAmount);
+    document.getElementById("totalInterest").innerHTML = `$${Number(totalInterestAmount).toFixed(2)}`;
+    
+    // Display total cost
+    document.getElementById("totalPrincipal").innerHTML = `$${Number(loanAmount).toFixed(2)}`;
 }
 
 // convert from annual percentage to a monthly percentage
@@ -26,16 +27,18 @@ function monthlyInterestRate(rate){
     let annualPercentageRate = ((rate / 100) / 12);
     return annualPercentageRate;
 }
-// total interest from principal
-function interestAmount(principal, apr){
-    let totalInterest = (principal * apr);
+// the total interest charged from the loan term  
+function interestAmount(totalAmount, principalAmount){
+    let totalInterest = (Number(totalAmount) - Number(principalAmount));
     return totalInterest;
 }
 // Total Loan Amount, principal and interest amount
-function totalAmount(principal, interestAmount){
-    let totalCost = (Number(principal) + Number(interestAmount));
+function totalAmount(loanTerm, monthlyPayment){
+    let totalCost = (Number(loanTerm) * Number(monthlyPayment));
     return totalCost;
 }
+
+// Display
 
 // the month(1 corresponding to the 1st month of payment, through the total number of months)
 
